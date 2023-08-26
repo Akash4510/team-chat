b5d9df865d169cb3e7b937fea4f91eed949ca695
Akash <ag308669@gmail.com>
2023-08-26 11:27:00 +0530

    Initial setup

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Hello Discord Clone</h1>
+      <p>This is a protected route</p>
+    </div>
+  );
+};
+
+export default Home;
