c0739cfc706ed08f681a1aa351ff29afd0404f6e
Akash <ag308669@gmail.com>
2023-08-26 22:22:46 +0530

    Added invite functionality

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,17 +1,24 @@
+import { Server } from '@prisma/client';
 import { create } from 'zustand';
 
-export type ModalType = 'createServer';
+export type ModalType = 'createServer' | 'invite';
+
+interface ModalData {
+  server?: Server;
+}
 
 interface ModalStore {
   type: ModalType | null;
+  data: ModalData;
   isOpen: boolean;
-  openModal: (type: ModalType) => void;
+  openModal: (type: ModalType, data?: ModalData) => void;
   closeModal: () => void;
 }
 
 export const useModal = create<ModalStore>((set) => ({
   type: null,
+  data: {},
   isOpen: false,
-  openModal: (type) => set({ type, isOpen: true }),
+  openModal: (type, data) => set({ type, data, isOpen: true }),
   closeModal: () => set({ type: null, isOpen: false }),
 }));
