a58a4f36465781d9aebd3637739aa0bac0576a4e
Akash <ag308669@gmail.com>
2023-08-27 06:21:55 +0530

    Added functionality to create conversations and display user on chat header

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from 'lucide-react';
 
 import MobileToggle from '@/components/mobile-toggle';
+import { UserAvatar } from '../user-avatar';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -11,10 +12,13 @@ interface ChatHeaderProps {
 
 const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
   return (
-    <div className="text-xl font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+    <div className="text-xl font-semibold px-3 flex items-center h-14 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobileToggle serverId={serverId} />
       {type === 'channel' && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 ml-3 mr-2" />
+      )}
+      {type === 'conversation' && (
+        <UserAvatar src={imageUrl} className="h-6 w-6 md:h-8 md:w-8 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
